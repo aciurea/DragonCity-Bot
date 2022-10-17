a2ecbfe17863b7bc3855e682bcd96ae7db15d36f
Alin Ciurea <alinci@playtika.com>
2022-10-17 20:21:04 +0300

    Improvements on utils

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
-from python_imagesearch.imagesearch import imagesearch, imagesearcharea
 import time
+
 import mouse
+from python_imagesearch import (imagesearch, imagesearch_loop,
+                                imagesearch_numLoop, imagesearcharea)
 
 
 def commonClaim():
@@ -19,52 +21,58 @@ def delay(seconds):
     time.sleep(seconds)
 
 def exists(value):
-    if (value[0] != -1):
-        return True
-    return False
+    return value[0] != -1;
 
 def checkIfCanClaim():
-    image = getImagePosition('./img/utils/ready_to_claim.png', 10)
-    if exists(image):
-        return image
-    time.sleep(3)
-    return checkIfCanClaim()
+    image = imagesearch_loop('./img/utils/ready_to_claim.png', 3)
+
+    return image if exists(image) else [-1]
 
 
 def getImagePosition(path, tries=20, precision=0.8, delay=0.5):
-    image = imagesearch(path, precision)
-    if(image[0] != -1): return image
+    image = imagesearch_numLoop(path, delay, tries, precision)
 
-    time.sleep(delay)
-    if (tries > 0):
-        return getImagePosition(path, tries - 1, precision)
-      
-    return [-1, -1]
+    return image if exists(image) else [-1]
 
-def moveAndClick(pos):
-    if exists(pos) == False:
-        return
-    mouse.move(pos[0], pos[1])
+def retry(fn, tries = 3):
+    if tries == 0: return [-1]
+
+    if(tries > 0):
+        img = fn()
+        if not exists(img):
+            return retry(fn, tries -1)
+        return img
+
+def moveAndClickOnIsland(pos, msg = 'Nothing to click'):
+    # TODO resolution is: 1600x900
+    moveAndClick(pos, msg)
+    delay(.5)
+    backBtn = retry(imagesearcharea('./img/fails/back.png', 0, 0, 500, 200), 2) # start at 0, 0, and end at 500, 200
+    moveAndClick(backBtn)
+    closeBtn = retry(imagesearch( './img/utils/close.png', 500, 0, 1600, 450), 2) # start at 0, 0, and end at 1600, 450. (most right horizonatlly, half the screen vertically)
+    moveAndClick(closeBtn)
+    
+
+def moveAndClick(pos, msg = 'Nothing to click'):
+    if not exists(pos):
+        return print(msg)
+        
+    moveTo(pos)
     
     while(mouse.get_position()[0] != pos[0]):
-        time.sleep(0.5)
+       delay(0.2)
 
     time.sleep(0.2)
     mouse.click()
 
 def closePopup(): 
     closeBtn = getImagePosition('./img/utils/close.png')
-   
-    if(closeBtn[0] == -1):
-        print('no close button')
+    moveAndClick(closeBtn, 'no close button')
 
-    moveAndClick(closeBtn)
 def closeVideo():
     closeBtn = getImagePosition('./img/utils/close_video.png')
-    if(closeBtn[0] == -1): 
-        return print('Close video button not found')
-        
-    moveAndClick(closeBtn)
+
+    moveAndClick(closeBtn, 'Close video button not found')
 
 
 def moveTo(position):
