74f6dc371c1ae2e5f79ff455ae908836d7672f90
aciurea <alinciurea90@gmail.com>
2022-11-02 12:25:31 +0200

    fixed quest not starting for the ones that are on the right most of the screen

--- quest.py
+++ quest.py
@@ -1,76 +1,88 @@
-import mouse
 from league import goToFight
 from rewards import openChest
-from utils import ThreadWithReturnValue, closePopup, delay, exists, moveAndClick, getImagePositionRegion
+from utils import ThreadWithReturnValue, closePopup, delay, exists, go_back, moveAndClick, getImagePositionRegion, scroll
 
 
-def getQuest(retries=2):
-    if retries == 0:
-        return [-1]
+def getQuests():
     path = './img/battle/start_quest1.png'
 
+    paths = []
     list = []
     step_px = 500
     for i in range(3):
         start, end = [i * step_px, i * step_px + step_px]
-        list.append(ThreadWithReturnValue(target=getImagePositionRegion, args=(path, start, 600, end, 800, .5, 3)).start())
+        list.append(ThreadWithReturnValue(target=getImagePositionRegion, args=(path, start, 600, end, 800, .5, 2)).start())
 
-  
     for thread in list:
         quest = thread.join()
-      
-        if exists(quest):
-            print('Quest is::: ', quest)
-            return [quest[0], quest[1] - 100]
 
-    mouse.drag(1500, 450, 800, 450, True, .5)
-    return getQuest(retries - 1)
+        if not exists(quest): continue;
+        if quest in paths: continue
+        paths.append([quest[0], quest[1]- 200])
 
+    return paths
 
-def battle():
-    quest = getQuest()
-
-    if not exists(quest):
-        return print('No quest fight available')
-    moveAndClick(quest)
-
-    ## TODO update the coordonates
+def open_battle():
     battle = getImagePositionRegion(
-        './img/battle/go_battle.png', 650, 700, 1000, 850)
+        './img/battle/go_battle.png', 650, 700, 1000, 850, .8, 2)
     if not exists(battle):
-        return print('No quest Battle')
-
+        print('No quest Battle')
+        return [-1]
     moveAndClick(battle)
-
     delay(1)
     battle = getImagePositionRegion(
-        './img/battle/go_to_battle.png', 600, 600, 900, 750)
+        './img/battle/go_to_battle.png', 600, 600, 900, 750, .8, 2)
+    if not exists(battle):
+        print('No go Battle btn available')
+        return [-1]
+    moveAndClick(battle)
+    delay(1)
+    battle =  getImagePositionRegion('./img/battle/go_to_battle.png', 600, 600, 900, 750, .8, 4)
+    if exists(battle): 
+        print('No go battle btn available')
+        return [-1]
+    return [1]
 
+def open_quest():
+    def inner_quest():
+        quests = getQuests()
 
-    if exists(battle):
-        print('battle 2', battle)
-        moveAndClick(battle)
-        delay(3)
-        battle =  getImagePositionRegion('./img/battle/go_to_battle.png', 600, 600, 900, 750, .8, 2)
-        if exists(battle):
+        for quest in quests:
+            if not exists(quest): continue
+            moveAndClick(quest)
             delay(1)
-            closePopup()
-            return print('The battle didnt start')
-        delay(3) # wait for the battle to start
-        goToFight()
-        openChest()
-        return
-   
+            battle = open_battle()
+            if exists(battle): 
+                print('Battle started')
+                return [1]
+            go_back()
+            delay(.5)
+            go_back()
+        return [-1]
+    quest = inner_quest()
+    if exists(quest): 
+       return quest
 
+    print('is it necessary to scroll???')
+    delay(1)
+    scroll([1550, 450], [9, 450])
+    delay(1)
+    quest = inner_quest()
+    if exists(quest):
+        return quest
+      
+    return [-1]
 
 def openQuestPanel():
-    quest = getImagePositionRegion('./img/battle/quest.png', 650, 200, 1000, 500)
+    quest_panel = getImagePositionRegion('./img/battle/quest.png', 650, 200, 1000, 500)
 
-    if not exists(quest):
+    if not exists(quest_panel):
         return print('Quest is not available')
 
     print('Start to battle...')
-    moveAndClick(quest)
-    battle()
+    moveAndClick(quest_panel)
+    if exists(open_quest()):
+        goToFight()
+        openChest()
     delay(.5)
     closePopup()
