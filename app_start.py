dd516d6dd19c306597196be494ec98f3bce9de45
aciurea <alinciurea90@gmail.com>
2022-11-11 12:09:39 +0200

    added app_start.py and test.py

---
+++ app_start.py
@@ -0,0 +1,36 @@
+# feeling lucky
+feeling_lucky = {
+    'x1': 642,
+    'x2': 955,
+    'y1': 705,
+    'y2': 805,
+}
+# delay 3s
+card = {
+    'x1': 500,
+    'x2': 1000,
+    'y1': 320,
+    'y2': 440,
+}
+
+# delay 3
+# close popup
+# delay 1s
+# claim_yellow
+claim_btn = {
+    'x1': 670,
+    'x2': 930,
+    'y1': 710,
+    'y2': 795,
+}
+# open chest, might be
+    #tap to open
+    #claim
+    # x1: 690, x2: 920, y1: 650, y2: 765
+
+#delay1s
+# daily claim, same as claim_yellow
+# delay1s
+# open chest()
+# delay1s
+# closePopup
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-from utils import delay, moveTo
-
+from utils import delay, exists, get_text, getImagePositionRegion, moveTo
+import mouse
+import keyboard as K
+from pynput import keyboard
 
 def moveY():
     delay(2)
@@ -9,14 +11,27 @@ def moveY():
         y += 100
         delay(.8)
 
-def moveX():
-    delay(2)
-    x = 600
-    while(x < 1601):
-        moveTo([x, 700])
-        x += 100
-        delay(.8)
+# def moveX():
+#     delay(2)
+#     x = 0
+#     while(x < 1601):
+#         moveTo([x, 850])
+#         x += 100
+#         delay(.8)
 
-moveX()
+# moveX()
 
-# battle 400, 600
+def get_pos():
+    while 1:
+        if K.is_pressed('p'):  # if key 'q' is pressed 
+            print('mouse pos',mouse.get_position())
+            # get_text()
+        if K.is_pressed('q'):
+            break  # if user pressed a key other than the given key the loop will break
+         
+        delay(.2)
+
+get_pos()
+
+# delay(3)
+# get_text()
