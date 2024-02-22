5f92e325b532f55625898594e83002f557cd5217
Alin <alinciurea90@gmail.com>
2024-02-22 12:16:07 +0200

    Removed unnecessary images from TV

--- test.py
+++ test.py
@@ -1,42 +1,46 @@
 from screeninfo import get_monitors
-from breed import sellEgg
-from freeze import _freeze_dragons, freeze_dragons
-from utils import delay, get_text, getImagePositionRegion, moveTo
+# from breed import sellEgg
+# from utils import delay, moveTo
 import mouse
 import keyboard as K
 import time
 import pyautogui
 
+from move import moveAndClick
+from utils import dragMapToCenter
+
+def delay(seconds):
+    if seconds < 0:
+        seconds = 0
+    time.sleep(seconds)
+
 def moveY():
     delay(2)
     y = 0
     while(y < 901):
-        moveTo([750, y])
+        # moveTo([750, y])
         y += 100
         delay(.8)
-
-
 
 def get_pos():
     start = time.time()
     while 1:
+
+        if K.is_pressed('c'):
+            dragMapToCenter()
         if K.is_pressed('p'):  # if key 'q' is pressed 
-            # collect_gems( dragMapToCenter())
-            # dragMapToCenter()
-            # doHeroicRace()
-            [ res ] = get_monitors()
-            print(res.width, res.height, res.width_mm, res.height_mm)
+          
+            # [ res ] = get_monitors()
+            # print(res.width, res.height, res.width_mm, res.height_mm)
             print('mouse pos',mouse.get_position())
+          
             # print(get_text(oponent=False))
             # print(get_text(oponent=True))
-            _freeze_dragons(-1, 313707)
-            _freeze_dragons(999999991, 285192)
+            # _freeze_dragons(-1, 313707)
+            # _freeze_dragons(999999991, 285192)
             # get_text()
-        if K.is_pressed('t'):
-            print(get_text())
-        if K.is_pressed('l'):
-            freeze_dragons()
-            print()
+        # if K.is_pressed('t'):
+        #     print(get_text())
         if K.is_pressed('s'):
             print(time.time() - start)
         if K.is_pressed('q'):
