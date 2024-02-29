d004b2f09abda4a5a7949184cc779de10d19c1ad
Alin <alinciurea90@gmail.com>
2024-02-29 09:24:21 +0200

    close the app

--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,14 @@
 import time
 
 from do_work import start_working
+from open import close_app
 
 while True:
     try:
         print("Starting the application...")
         start_working()
     except Exception as e:
+        close_app()
         print("Script crashed, restarting...", e)
     finally:
             one_hour = 1800
