5ec5bf2bbccb407a0f21742e615667464b026ed4
Alin <alinciurea90@gmail.com>
2024-07-19 12:36:36 +0300

    extension updates

--- browser/main.js
+++ browser/main.js
@@ -1,28 +1,41 @@
-console.log('main.js loaded');
-
-const delay = (fn, time) => {
-    const id = setTimeout(() => {
-        fn();
-        clearTimeout(id);
-    }, time);
+const delay = (time) => {
+    return new Promise((resolve) => {
+        const id = setTimeout(() => {
+            resolve();
+            clearTimeout(id);
+        }, time);
+    });
+    
 };
 
-const elements = document.querySelectorAll('[data-testid="list-view-buy-button"]');
+(async () => {
+    let times = 7;
 
-elements?.forEach((el) => {
-    if(!el?.textContent?.includes("€")) {
-        el?.click();
-    } else {
-        console.log(el?.textContent);
+    while (times > 0) {
+        times--;
+        const elements = document.querySelectorAll('[data-testid="list-view-buy-button"]');
+
+        if(!elements?.length) {
+            await delay(1000);
+            continue;
+        }
+
+        elements?.forEach((el) => {
+            if(!el?.textContent?.includes("€")) {
+                console.log('clicking on element');
+                el?.click();
+            } else {
+                console.log(el?.textContent);
+            }
+        });
+
+        times = 0;
     }
-});
 
-console.log(sessionStorage);
-
-delay(() => {
-    const key =  "dc-automation";
+    await delay(1000)
+    const key = "dc-automation";
     if(sessionStorage.getItem(key) === "true") return;
 
     window.location.href = "https://www.dragoncitygame.com/";
     sessionStorage.setItem(key, "true");
-}, 3000);
+})();
