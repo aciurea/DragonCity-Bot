a8f45c3378b92c36a04bd4af45c51ed007b1a166
Alin <alinciurea90@gmail.com>
2024-08-30 11:50:19 +0300

    Improved Arena change defeated dragon by using text to accuratelly match the position where I need to click

--- arena.py
+++ arena.py
@@ -1,4 +1,8 @@
 import time
+import concurrent.futures
+
+import constants as C
+
 from battle import Battle
 from close import check_if_ok
 from move import multiple_click
@@ -11,8 +15,8 @@ from utils import (
                 getImagePositionRegion,
                 get_int,
                 moveAndClick)
-import constants as C
-import concurrent.futures
+from screen import Screen
+
 
 
 jsonPos = {
@@ -48,12 +52,19 @@ class Arena:
 
     @staticmethod
     def prepare_fight():
-        if exists(Arena.get_dragon_defeated_pos()):
-            change_btn = getImagePositionRegion(C.ARENA_CHANGE, *Arena.mon_quarters['bottom_left'], .8, 1)
-            if not exists(change_btn): return print('Change button not found')
-            moveAndClick(change_btn)
-            delay(1)
-            Arena.change_defetead_dragon()
+        bbox = [360, 700, 1175, 780]
+        text_positions = Screen.get_text_pos(bbox)
+
+        if len(text_positions) == 3: return
+        
+        bbox = [650, 1140, 865, 1240]
+        text_positions = Screen.get_text_pos(bbox)
+
+        for t in text_positions:
+            if 'change' in t['text'].lower():
+                moveAndClick(t['position'])
+                delay(1)
+                Arena.change_defetead_dragon()
 
     @staticmethod
     def order_by_power():
@@ -67,33 +78,22 @@ class Arena:
         new_pos = [order_by_power_des[0] + 50, order_by_power_des[1] + 50]
         moveAndClick(new_pos)
 
-    def get_dragon_defeated_pos():
-        pos = [Arena.grid['x0'], Arena.grid['y2'], Arena.grid['x8'], Arena.grid['y5']]
-        
-        return getImagePositionRegion(C.ARENA_DEFETEAD_DRAGON, *pos, .8, 1)
-    
-    def is_defeated_dragon_in_select_team():
-        pos = [Arena.grid['x0'], Arena.grid['y3'], Arena.grid['x7'], Arena.grid['y5']]
-        
-        return exists(getImagePositionRegion(C.ARENA_DEFETEAD_DRAGON_SELECT_TEAM, *pos, .8, 1))
-    
-    def get_select_dragon_btn():
-        pos = [Arena.grid['x0'], Arena.grid['y4'], Arena.grid['x7'], Arena.grid['y5']]
-
-        return getImagePositionRegion(C.ARENA_SELECT_DRAGON, *pos, .8, 1)
 
     @staticmethod
     def change_defetead_dragon():
-        start =  time.time()
+        bboxes = [
+            [510, 1040, 710, 1110],
+            [1330, 1040, 1530, 1110],
+            [2155, 1040, 2345, 1110],
+        ]
 
-        while time.time() - start < 60: # 1 minute
-            if not Arena.is_defeated_dragon_in_select_team():
-                check_if_ok()
-                return print('Dragons are ready for fight')
-            select_new_dragon_btn = Arena.get_select_dragon_btn()
-            moveAndClick(select_new_dragon_btn)
+        for bbox in bboxes:
+            text_positions = Screen.get_text_pos(bbox)
+            print(text_positions)
+            if  len(text_positions) > 0 and 'enhance' in text_positions[0]['text'].lower(): continue
+
+            moveAndClick([bbox[0], bbox[1]])
             delay(1)
-
             filter_dragons = getImagePositionRegion(C.ARENA_FILTER_DRAGONS, *Arena.mon_quarters['4thRow'], .8, 1)
             
             if not exists(filter_dragons): return print('Filter dragons button not found')
@@ -105,12 +105,8 @@ class Arena:
             new_dragon = getImagePositionRegion(C.ARENA_NEW_DRAGON, *Arena.mon_quarters['2ndRow'], .8, 1)
             if not exists(new_dragon): raise Exception('No dragons available')
             moveAndClick(new_dragon)
-            # TODO remove it
-            temp = [1812, 969]
-            delay(.1)
-            moveAndClick(temp)
             delay(1)
-        raise Exception('No more dragons to fight. Exit immediatelly')
+        check_if_ok()
 
     @staticmethod
     def get_fight_btn():
@@ -148,6 +144,7 @@ class Arena:
         delay(2)
         Arena._check_attack_report()
         delay(1)
+        Arena._check_season_end()
         Arena.claim_rush()
         delay(1)
         Arena.wait_for_the_fight_tab()
@@ -212,4 +209,13 @@ class Arena:
         if exists(Arena.get_fight_btn()): return 
 
         check_if_ok() # first we hit the close button
+
+    @staticmethod
+    def _check_season_end():
+        bbox = [1050, 1100, 1480, 1225]
+        text_positions = Screen.get_text_pos(bbox)
+
+        for t in text_positions:
+            if 'start' in t['text'].lower():
+                moveAndClick(t['position'])
         
