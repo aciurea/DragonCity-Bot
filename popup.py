9e1b8d4063ec54b95cdb41ece647148b3cc7850e
Alin <alinciurea90@gmail.com>
2024-09-19 13:43:28 +0300

    Refactored popup chest to use easyocr.
    Simplified the way it finds popup chest.
    Need to rename it to chest instead of popup.

--- popup.py
+++ popup.py
@@ -1,57 +1,40 @@
-import time
-from screeninfo import get_monitors
 from move import moveAndClick, multiple_click
-from utils import exists, get_monitor_quarters, getImagePositionRegion, get_int
-import constants as C
-import concurrent.futures
+from utils import exists
 from timers import delay
+from screen import Screen
 
-res = get_monitors()[0]
-_percent = { "claim_btn": [.490625, .79652], "tap_btn": [.494140625, .694] }
-
-_jsonPos =  { 
-    "TAP_BTN": [get_int(res.width * _percent["tap_btn"][0]), get_int(res.height * _percent["tap_btn"][1])],
-    "CLAIM_BTN": [get_int(res.width * _percent["claim_btn"][0]), get_int(res.height * _percent["claim_btn"][1])],
+text = {
+    'tap': 'tap',
+    'claim': 'claimi'
 }
 
 class Popup:
-    mon_quarters = get_monitor_quarters()
-
-    @staticmethod
-    def _get_chest():
-        sections = [
-            [C.POPUP_LEFT_HEADER, *Popup.mon_quarters['1stHorHalf']],
-            [C.POPUP_TAP, *Popup.mon_quarters['2ndHorHalf']],
-        ]
-    
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            result_list = executor.map(lambda args: getImagePositionRegion(*args, .8, 2), sections)
-            for btn in result_list:
-                if exists(btn): return btn
-            return [-1]
-        
-    @staticmethod
-    def _claim_chest():
-        claim_btn = getImagePositionRegion(C.POPUP_CLAIM, *Popup.mon_quarters['2ndHorHalf'], .8, 5, 1)
-
-        if exists(claim_btn):
-            multiple_click(claim_btn, 3)
-            print('Chest opened!')
-        else: moveAndClick(_jsonPos['CLAIM_BTN'])
-        delay(5) # Opening popup is kind of slow, just wait 5 seconds to be safe.
-        
-    @staticmethod
-    def _open_chest():
-        tab_btn = getImagePositionRegion(C.POPUP_TAP, *Popup.mon_quarters['3rdRow'], .8, 1)
-        if exists(tab_btn): 
-          multiple_click(tab_btn, times=10)
-        else:
-            print("Tap not found")
-            multiple_click(_jsonPos['TAP_BTN'], times=10)
-        Popup._claim_chest()
-
     @staticmethod
     def check_popup_chest():
-        if exists(Popup._get_chest()):
-          print("Chest found, try to open it.")
-          Popup._open_chest()
+        chest_pos = Popup._get_chest()
+
+        if not exists(chest_pos): return
+       
+        multiple_click(chest_pos, 5, 0.01)
+        delay(2)
+        moveAndClick(Popup._get_claim_btn())
+            
+    @staticmethod
+    def _get_chest():
+        bbox = [0.40078125, 0.6451389, 0.4734375, 0.7548612]
+
+        for t in Screen.get_text_pos(bbox):
+            if Screen.is_match_with_one_difference(text['tap'], t['text']): return t['position']
+        return [-1]
+
+    @staticmethod
+    def _get_claim_btn(times = 3):
+        if times < 1:  return Screen.get_pos([0.487890625, 0.79723])# static pos
+        bbox = [0.444140625, 0.754861, 0.550390625, 0.834]
+
+        text_positions = Screen.get_text_pos(bbox, gray_mode=True)
+
+        for t in text_positions:
+            if Screen.is_match_with_one_difference(text['claim'], t['text']): return t['position']
+        delay(1)
+        return Popup._get_claim_btn(times - 1)
