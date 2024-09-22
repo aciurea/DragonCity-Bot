e46a082846e1f1274a624721bd94e0172b2eb8fc
Alin <alinciurea90@gmail.com>
2024-09-22 09:43:42 +0300

    Refactored shop

--- shop.py
+++ shop.py
@@ -1,40 +1,30 @@
-import time
-
 from pyautogui import scroll
 from close import check_if_ok
-from move import moveTo, multiple_click
-from utils import moveAndClick, delay, exists
+from move import multiple_click, moveAndClick
+from utils import delay
 from position_map import Position_Map
+from screen import Screen
+
 
 class Shop:
-    last_time_started = 0
-    wait_time = 3600 * 3 
-    pos = {
-        "shop": [2440, 1250],
-        "orbs": [1280, 1090],
-        "vertical_scroll": [2560, 865],
-        "buy_heroic": [2220, 670],
-        "buy_legendary": [2215, 1190]
-    }
-
-    def go_to_orbs():
-        scroll_pos = Shop.pos['vertical_scroll']
-        moveTo(scroll_pos)
-        scroll(-10_000)
-        delay(.5)
+    shop_pos = Screen.get_pos([0.9552083,  0.862037])
+    orbs_pos = Screen.get_pos([0.49427083, 0.74])
+    heroic_pos = Screen.get_pos([0.86, 0.471296])
+    legendary_pos = Screen.get_pos([0.86,  0.82])
 
     def open_shop():
-        if time.time() - Shop.last_time_started < Shop.wait_time: return
+        actions = [
+            Position_Map.center_map,
+            lambda: delay(.5),
+            lambda: moveAndClick(Shop.shop_pos),
+            lambda: moveAndClick(Shop.orbs_pos),
+            lambda: scroll(-10_000),
+            lambda: delay(.5),
+            lambda: multiple_click(Shop.heroic_pos, 5, 0.1),
+            lambda: multiple_click(Shop.legendary_pos, 5, 0.1),
+            check_if_ok,
+        ]
 
-        Position_Map.center_map()
-        delay(1)
-        moveAndClick(Shop.pos['shop'])
-        delay(.5)
-        moveAndClick(Shop.pos['orbs'])
-        delay(.5)
-        Shop.go_to_orbs()
-        
-        multiple_click(Shop.pos['buy_heroic'], 6)
-        multiple_click(Shop.pos['buy_legendary'], 6)
-        check_if_ok()
-        Shop.last_time_started = time.time()
+        for action in actions:
+            action()
+            delay(.5)
