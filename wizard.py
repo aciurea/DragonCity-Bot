853f908acbf445f395ac26046c5f7dd56aadb310
Alin <alinciurea90@gmail.com>
2024-09-23 18:38:26 +0300

    Update wizard

--- wizard.py
+++ wizard.py
@@ -1,22 +1,29 @@
-import constants as C
 import random
 
 from close import check_if_ok
 from position_map import Position_Map
-from utils import delay, get_monitor_quarters, get_int, getImagePositionRegion, exists
-from move import moveAndClick, multiple_click, moveTo
+from utils import delay, exists
+from move import moveAndClick, multiple_click
+from screen import Screen
 
 from screeninfo import get_monitors
 
+text = {
+    'play': 'play',
+    'claim': 'claimi',
+    'claim&quit': 'claim',
+    'claimall': 'claimall'
+}
+
+
 class Wizard:
-    _mon_quarters = get_monitor_quarters()
     res = get_monitors()[0]
-    _quit_pos = [get_int(0.30585 * res.width), get_int(0.81 * res.height)]
-    wizard_static_pos = [get_int(res.width * .82929) , get_int(res.height * .334027)]
+    # _quit_pos = [get_int(0.30585 * res.width), get_int(0.81 * res.height)]
+    wizard_static_pos = Screen.get_pos([0.8203125, 0.14723])
 
     @staticmethod
     def open_wizard():
-        Position_Map.drag_map_to_the_bottom()
+        Position_Map.center_map()
         delay(1)
         moveAndClick(Wizard.wizard_static_pos)
         delay(1)
@@ -29,65 +36,94 @@ class Wizard:
         moveAndClick(play_btn)
         delay(10)
         Wizard._play_wizard()
+        Wizard._claim_and_quit_wizard()
         check_if_ok()
-    
+
     @staticmethod
     def _play_wizard():
-        times = 2
-    
-        balls = [[842, 665],
-                [1126, 777],
-                [1470, 781],
-                [1737, 646]
+        tries = 3
+
+        balls = [
+            Screen.get_pos([0.3322916, 0.427]),
+            Screen.get_pos([0.4354167, 0.5138]),
+            Screen.get_pos([0.56875, 0.512963]),
+            Screen.get_pos([0.66979167, 0.42962963])
         ]
-        
-        while times >= 0:
-            if not exists(Wizard._get_claim_btn()):
-                check_if_ok()
-                delay(1)
-                moveAndClick(Wizard._quit_pos)
-                return
-            
-            times -= 1
+
+        while tries >= 0:
+            tries -= 1
             ball = random.choice(balls)
             multiple_click(ball)
-            delay(2)
-            
+            delay(7)
+
             claim_btn = Wizard._get_claim_btn()
-            
+
             if not exists(claim_btn):
                 check_if_ok()
-            
-            moveAndClick(claim_btn)
-            delay(1)
-            claim_and_quit = Wizard._get_claim_and_quit()
-            
-            if not exists(claim_and_quit):
-                moveAndClick([955, 1181])
-                
-            moveAndClick(Wizard._get_claim_and_quit())
-            claim_all = Wizard._get_claim_all_btn()
-            
-            if not exists(claim_all):
-                moveAndClick([1150, 1129])
-            moveAndClick(claim_all)
-        
+                # TODO check the flow when the wizard is displayed
+                return
+
+    @staticmethod
+    def _claim_and_quit_wizard():
+        # TODO check it.
+        claim_btn = Wizard._get_claim_btn()
+        if not exists(claim_btn):
+            check_if_ok()
+            return
+
+        moveAndClick(claim_btn)
+        delay(1)
+        claim_and_quit = Wizard._get_claim_and_quit()
+
+        if not exists(claim_and_quit):
+            check_if_ok()
+            return
+
+        moveAndClick(Wizard._get_claim_and_quit())
+        claim_all = Wizard._get_claim_all_btn()
+
+        if not exists(claim_all):
+            check_if_ok()
+            return
+        moveAndClick(claim_all)
+
     @staticmethod
     def _get_claim_all_btn():
-        return getImagePositionRegion(C.WIZARD_YELLOW_CLAIM, *Wizard._mon_quarters['4thRow'], .8, 2)
-    
+        bbox = [0.44583, 0.77, 0.55364583, 0.8287037]
+
+        text_positions = Screen.get_text_pos(bbox)
+
+        for t in text_positions:
+            if Screen.is_match_with_one_difference(text['claimall'], t['text']):
+                return t['position']
+        return [-1]
+
     @staticmethod
     def _get_claim_and_quit():
-        return getImagePositionRegion(C.WIZARD_RED_CLAIM, *Wizard._mon_quarters['4thRow'], .8, 2)
-        
+        bbox = [0.30364583, 0.781481481, 0.3734375, 0.8481481]
+        text_positions = Screen.get_text_pos(bbox)
+
+        for t in text_positions:
+            if Screen.is_match_with_one_difference(text['claim&quit'], t['text']):
+                return t['position']
+        return [-1]
+
     @staticmethod
     def _get_claim_btn():
-        return getImagePositionRegion(C.WIZARD_CLAIM_BTN, *Wizard._mon_quarters['4thRow'], .8, 2)
-            
-    # @staticmethod
-    # def _get_wizard_gem():
-    #     return getImagePositionRegion(C.WIZARD_GEMS_BTN, *Wizard._mon_quarters['2ndHorHalf'], .8, 2)
-    
+        bbox = [0.849479167, 0.8527, 0.9302083, 0.9231481]
+        text_positions = Screen.get_text_pos(bbox)
+
+        for t in text_positions:
+            if Screen.is_match_with_one_difference(text['claim'], t['text']):
+                return t['position']
+        return [-1]
+
     @staticmethod
     def _get_play_btn():
-        return getImagePositionRegion(C.WIZARD_PLAY_BTN, *Wizard._mon_quarters['2ndHorHalf'], .8, 2)
+        bbox = [0.46510416, 0.737, 0.53, 0.80278]
+
+        text_positions = Screen.get_text_pos(bbox)
+
+        for t in text_positions:
+            if Screen.is_match_with_one_difference(text['play'], t['text']):
+                return t['position']
