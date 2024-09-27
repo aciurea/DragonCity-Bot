92aba34d0ca0d44f02ffdc40ed6d4f783e2e4fa5
Alin <alinciurea90@gmail.com>
2024-09-27 12:12:45 +0300

    Small improvements on scheduler.

--- scheduler.py
+++ scheduler.py
@@ -1,22 +1,22 @@
 from do_work import start_working
-from open import close_app
+from open import OpenApp
 from datetime import datetime
 from PIL import ImageGrab
 
 from git import update_project
-from utils import delay
 from mail import Mail
 
 try:
     print("Starting the application...")
     work_message = start_working()
 
-    message="[Success] ... " + datetime.now().strftime("%Y-%m-%d %H:%M:%S \n\n") + work_message
+    message = "[Success] ... " + datetime.now().strftime("%Y-%m-%d %H:%M:%S \n\n") + work_message
     Mail.send(message=message, subject="DC")
 except Exception as e:
     print('[Error is]: ', e)
     message = '[Error is]: ' + str(e) + " " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     ImageGrab.grab().save('./log/' + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '.png')
     Mail.send(message=message, subject="DC")
-close_app()
+
+OpenApp._close_app()
 update_project()
