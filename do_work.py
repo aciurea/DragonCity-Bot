05cf23b4607f9455335b0daf15e286b7ae25952e
Alin <alinciurea90@gmail.com>
2024-10-04 18:00:07 +0300

    Added activated towers to do_work

--- do_work.py
+++ do_work.py
@@ -4,21 +4,21 @@ from arena import Arena
 from breed import Breed
 from claim_all import claim_all
 from close import check_if_ok
-from collectFood import collect_food
-from collectGold import collectGold
-from daily_collect import Daily_Collect
+from farm import Farm
+from gold import Gold
+from daily_browser_collect import Daily_Browser_Collect
 from habitat import Habitat
 from heroic import Heroic
 from league import League
-from open import open_app, close_app
+from open import OpenApp
 from orbs import Orbs
 from quest import Quest
 from runner import Runner
 from shop import Shop
-from towers import activate_towers
+from towers import Towers
 from tv import TV
 from utils import get_int
-from daily_treasure import collect_daily_treasure
+from daily_treasure import Daily_Treasure
 from events_collection import Events_Collection
 from wizard import Wizard
 from move import center_map
@@ -27,33 +27,36 @@ from clear_event import ClearEvent
 
 import time
 
+# default screen resolution when no monitor is connected is 1024x768
+
+
 def start_working():
     do_work = [
-        open_app,
+        OpenApp.open_app,
         claim_all,
-        activate_towers,
-        collect_food,
-        collectGold,
+        Towers.activate_towers,
+        Farm.collect_food,
+        Gold.collectGold,
         Orbs.collect_orbs,
-        Events_Collection.collect_events,
         Shop.open_shop,
-        collect_daily_treasure,
-        Daily_Collect.collect,
+        Daily_Treasure.collect_daily_treasure,
+        Daily_Browser_Collect.collect_daily_streak,
         Alliance.open_alliance,
         Arena.enter_battle,
+        League.enter_league,
 
         ClearEvent.skip_event,
-        Heroic.race,
-        Quest.open_quest,
-        Runner.run,
+        # Heroic.race,
+        # Quest.open_quest,
+        # Runner.run,
         Wizard.open_wizard,
         Puzzle.open_puzzle,
+        Events_Collection.collect_events,
 
         # TV.open_tv,
-        Habitat.buy_habitat,
+        # Habitat.buy_habitat,
         # More work can be added here
-        # Tree of life
-        close_app
+        OpenApp._close_app,
     ]
     str_actions = "Actions with time are: \n\n"
 
@@ -67,4 +70,3 @@ def start_working():
         check_if_ok()
         center_map()
     return str_actions
-    
