bdaa6ba8d46029e68b688cea33e206101f8f9d02
Alin <alinciurea90@gmail.com>
2024-10-04 13:11:46 +0300

    main.js updated

--- browser/main.js
+++ browser/main.js
@@ -5,7 +5,7 @@ const delay = (time) => {
             clearTimeout(id);
         }, time);
     });
-    
+
 };
 
 const URLs = Object.freeze({
@@ -13,28 +13,36 @@ const URLs = Object.freeze({
     dailyStreak: "https://www.dragoncitygame.com/dragon-city-daily-streak",
 });
 const key = "finished";
+const collected = "collected";
 const url = "url";
 
 (async () => {
     const storedUrl = sessionStorage.getItem(url);
-    
-    if(storedUrl === null) {
+
+    if (storedUrl === null) {
         sessionStorage.setItem(url, URLs.dailyStreak);
         window.location.href = URLs.dailyStreak;
         console.log('redirect to dailyStreak');
         return;
     } else sessionStorage.setItem(url, URLs.store);
-    
+
     await delay(2_000);
 
     const elements = document.querySelectorAll('[data-testid="list-view-buy-button"]');
 
     elements?.forEach((el) => {
-        if(!el?.textContent?.includes("€")) el?.click();
+        if (el?.textContent?.toLowerCase().includes("claim")) {
+            el?.click();
+            sessionStorage.setItem(collected, "true");
+        }
         else console.log(el?.textContent);
     });
- 
-    if(sessionStorage.getItem(key) === "true") return;
+
+    if (sessionStorage.getItem(key) === "true") {
+        const url = sessionStorage.getItem(collected) === "true" ? "http://localhost:8080/collect" : "http://localhost:8080/stop";
+        fetch(url);
+        return;
+    }
 
     sessionStorage.setItem(key, "true");
     window.location.href = URLs.store;
